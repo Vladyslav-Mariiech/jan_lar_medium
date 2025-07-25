27f7ed7666f4139a2f53b50b843c386f10cc5838
Vladyslav <mvi110601@gmail.com>
2025-07-25 14:04:14 +0300

    index.blade add photo created

---
+++ resources/views/photo/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+                <div class="panel-body">
+                    @include('common.errors')
+                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="task" class="col-sm-3 control-label">Photo</label>
+                            <div class="col-sm-6">
+                                <input type="file" name="name" id="photo-name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Photo
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
